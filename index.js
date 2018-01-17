749226437b3e15821eb2afd438bb7b3c8b3246b3
luoxue <1904240744@qq.com>
2018-01-17 17:22:42 +0800

    use all, get...

--- index.js
+++ index.js
@@ -2,19 +2,29 @@ const express = require('express')
 const app = new express()
 
 /**
- * 中间件（middleware）就是处理HTTP请求的函数
- * @param {*} request HTTP请求
- * @param {*} response HTTP回应
- * @param {*} next 调用下一个中间件，回调函数
+ * all()所有请求都必须通过该中间件
+ * 参数 * 表示对所有路径有效
+ * get方法则是只有GET动词的HTTP请求通过该中间件，它的第一个参数是请求的路径
  */
-const uselessMiddleware = (request, response, next) => {
-    console.log(request.url)
+app.all('*', (request, response, next) => {
+    response.writeHead(200, {'Content-type': 'text/plain'})
     next()
-}
+})
 
-app.use('/', uselessMiddleware)
-app.use('/about', (request, response) => {
-    response.send('about')
+app.get('/', (request, response, next) => {
+    response.end('index')
+})
+
+/**
+ * 可以匹配/about/luoxue, request.params.name = luoxue
+ * 最后跟 ? 表示参数可选
+ */
+app.get('/about/:name?', (request, response) => {
+    response.end(request.params.name)
+})
+
+app.get('*', (request, response) => {
+    response.end('404')
 })
 
 app.listen(4000)
