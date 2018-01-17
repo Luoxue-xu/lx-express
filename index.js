0c172dcf74e2f02ef67fa4721d35470c82763c9b
luoxue <1904240744@qq.com>
2018-01-17 17:47:51 +0800

    use redirect, sendFile

--- index.js
+++ index.js
@@ -2,29 +2,18 @@ const express = require('express')
 const app = new express()
 
 /**
- * all()所有请求都必须通过该中间件
- * 参数 * 表示对所有路径有效
- * get方法则是只有GET动词的HTTP请求通过该中间件，它的第一个参数是请求的路径
+ * response.redirect 重定向
  */
-app.all('*', (request, response, next) => {
-    response.writeHead(200, {'Content-type': 'text/plain'})
-    next()
-})
-
 app.get('/', (request, response, next) => {
-    response.end('index')
+    response.redirect('/about')
 })
 
 /**
- * 可以匹配/about/luoxue, request.params.name = luoxue
- * 最后跟 ? 表示参数可选
+ * response.sendFile 发送文件
  */
-app.get('/about/:name?', (request, response) => {
-    response.end(request.params.name)
-})
-
-app.get('*', (request, response) => {
-    response.end('404')
+app.get('/about', (request, response, next) => {
+    response.sendFile('/public/img/bg.jpg')
+    response.end('about')
 })
 
 app.listen(4000)
